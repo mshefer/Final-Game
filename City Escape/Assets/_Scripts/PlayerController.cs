93e2981fb7fca60b025125b286e135c64ed7d494
mshefer <42977579+mshefer@users.noreply.github.com>
2018-12-02 19:06:05 -0500

    3

--- City Escape/Assets/_Scripts/PlayerController.cs
+++ City Escape/Assets/_Scripts/PlayerController.cs
@@ -2,15 +2,51 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class Boundary
+{
+    public float xMin, xMax, yMin, yMax;
+}
+
 public class PlayerController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private Rigidbody2D rb2d;
+    public float speed;
+    public float jumpForce;
+    public Boundary boundary;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        Vector2 movement = new Vector2(moveHorizontal, 0);
+        rb2d.AddForce(movement * speed);
+
+       rb2d.position = new Vector3
+       (
+              Mathf.Clamp(rb2d.position.x, boundary.xMin, boundary.xMax),
+              Mathf.Clamp(rb2d.position.y, boundary.yMin, boundary.yMax),
+               0.0f
+       );
+    }
+
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        if ( collision.collider.tag == "Ground")
+        {
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                rb2d.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+            }
+        }
+    }
 }
---
+++ City Escape/Assets/_Scripts/StarRotator.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StarRotator : MonoBehaviour
+{
+
+    
+    void Update()
+    {
+        
+        transform.Rotate(new Vector3(0, 0, 45) * Time.deltaTime);
+    }
+}
